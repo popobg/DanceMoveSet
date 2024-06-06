e89c72318a491a63b7c06dc088e7194cdd6ee6c9
popobg <pauline.bouyssou1@gmail.com>
2024-06-06 16:58:08 +0200

    Add solution

---
+++ Dance/DanceMoveSet/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace DanceMoveSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] moves = new string[32];
+
+            bool done = false;
+            int index = 0;
+
+            while (index < moves.Length && done == false)
+            {
+                Console.Write("Type a set move, or done if you're finished: ");
+                var userMove = Console.ReadLine();
+
+                while (userMove == "" || userMove == null)
+                {
+                    Console.Write("You didn't type anything. Please type a set move, or done if you're finished: ");
+                    userMove = Console.ReadLine();
+                }
+
+                if (userMove == "done")
+                {
+                    done = true;
+                }
+                else
+                {
+                    moves[index] = userMove;
+                    index++;
+                }
+            }
+
+            Console.WriteLine("How many times should we repeat the danse?");
+
+            int numberIteration = Convert.ToInt32(Console.ReadLine());
+
+            RepeatMoves(moves, numberIteration);
+        }
+
+        public static void RepeatMoves(string[] array, int iteration)
+        {
+            for (int i = 0; i < iteration; i++)
+            {
+                Console.WriteLine(string.Join(" ", array));
+                Console.WriteLine();
+            }
+        }
+    }
+}
